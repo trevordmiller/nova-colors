fabf7ff4cf51762bae9dfaac7d04e8d41347eb3e
Trevor D. Miller <trevordmiller@icloud.com>
2016-09-02 21:55:29 -0600

    Add test

--- index.test.js
+++ index.test.js
@@ -2,5 +2,6 @@ const novaColors = require('./index')
 
 test('hex colors are available', () => {
   expect(novaColors.normal.cyan).not.toBe('#000000')
+  expect(novaColors.normal.black).toBe( '#445660')
   expect(novaColors.normal.cyan).toBe('#7FC1CA')
 })
