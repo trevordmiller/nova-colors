392b6f4a6899dce1f455596b0a969bb60699c1f7
Trevor D. Miller <trevordmiller@icloud.com>
2016-09-16 21:04:59 -0600

    Refactor tests

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 const novaColors = require('./index')
 
 test('hex colors are available', () => {
-  expect(novaColors.normal.cyan).not.toBe('#000000')
   expect(novaColors.normal.black).toBe( '#445660')
   expect(novaColors.normal.cyan).toBe('#7FC1CA')
+  expect(novaColors.normal.cyan).not.toBe('#000000')
 })
