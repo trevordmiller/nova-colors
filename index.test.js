cd01e885d1095c826cdc8b161bfb4988b4d42584
Trevor D. Miller <trevordmiller@icloud.com>
2016-09-29 00:10:35 -0600

    Add clean API for color mappings (resolve #20) (#24)

--- index.test.js
+++
@@ -1,7 +0,0 @@
-const novaColors = require('./index')
-
-test('hex colors are available', () => {
-  expect(novaColors.normal.black).toBe( '#3C4C55')
-  expect(novaColors.normal.cyan).toBe('#7FC1CA')
-  expect(novaColors.normal.cyan).not.toBe('#000000')
-})
---
+++ src/index.test.js
@@ -0,0 +1,29 @@
+import colorValues, {
+  uiGroups,
+  syntaxGroups,
+  versionControlGroups,
+  ansiGroups,
+}  from './index'
+
+test('colors values', () => {
+  expect(colorValues.colors.cyan).toBe('#7FC1CA')
+  expect(colorValues.grays.gray4).toBe('#899BA6')
+})
+
+test('UI group mappings to color values', () => {
+  expect(uiGroups.userActionNeeded).toBe(colorValues.colors.red)
+  expect(uiGroups.gray5).toBe(colorValues.grays.gray5)
+})
+
+test('syntax group mappings to color values', () => {
+  expect(syntaxGroups.type).toBe(colorValues.colors.green)
+})
+
+test('version control group mappings to color values', () => {
+  expect(versionControlGroups.renamed).toBe(colorValues.colors.blue)
+})
+
+test('ansi group mappings to color values', () => {
+  expect(ansiGroups.normal.cyan).toBe(colorValues.colors.cyan)
+  expect(ansiGroups.bright.red).toBe(colorValues.colors.orange)
+})
